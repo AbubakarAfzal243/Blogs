eb69b7f5eb24eb38360a43eef5c37b960933d687
Abubakar <abubakar@ventechstudio.com>
2021-05-03 19:29:29 +0500

    Blogs Site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateBlogComponent } from './create-blog/create-blog.component';
+import { DetailBlogComponent } from './detail-blog/detail-blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+     path: 'create', component: CreateBlogComponent 
+  },
+  {
+     path: 'details', component: DetailBlogComponent 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateBlogComponent } from './create-blog/create-blog.component';
+import { DetailBlogComponent } from './detail-blog/detail-blog.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BlogsListComponent } from './blogs-list/blogs-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateBlogComponent,
+    DetailBlogComponent,
+    HeaderComponent,
+    FooterComponent,
+    BlogsListComponent
   ],
   imports: [
     BrowserModule,
